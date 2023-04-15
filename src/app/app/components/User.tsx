668ddc8ff7731c054bc9423f4f77a9ebf1784394
bernardo.amim <bernardo.amim@mercatus.com>
2023-04-14 23:27:14 -0300

    checking out cookies and headers

--- src/app/app/components/User.tsx
+++ src/app/app/components/User.tsx
@@ -1,6 +1,10 @@
+import { cookies, headers } from 'next/headers'
 import { Repo } from './Repo'
 
 export async function User() {
+  const userCookies = cookies()
+  const userHeaders = headers()
+
   const response = await fetch('https://api.github.com/users/bernardoamim', {
     cache: 'no-store', // SSR-like
     next: {
@@ -13,6 +17,9 @@ export async function User() {
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>User: {user.name}</h1>
       <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <pre>Cookies: {JSON.stringify(userCookies, null, 2)}</pre>
+      <pre>Headers: {JSON.stringify(userHeaders, null, 2)}</pre>
 
       {/** @ts-expect-error Async Server Component */}
       <Repo />
